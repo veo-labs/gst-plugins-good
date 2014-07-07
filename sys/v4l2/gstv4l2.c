22b0e992a095c57b8218bed9953db8a01af6067c
ayaka <ayaka@soulik.info>
2014-07-08 02:38:53 +0800

    v4l2: add register of gstv4l2videoenc
    This code will register gstv4l2videoenc

--- sys/v4l2/gstv4l2.c
+++ sys/v4l2/gstv4l2.c
@@ -42,6 +42,8 @@
 #include "gstv4l2src.h"
 #include "gstv4l2sink.h"
 #include "gstv4l2radio.h"
+#include "gstv4l2videodec.h"
+#include "gstv4l2videoenc.h"
 #include "gstv4l2deviceprovider.h"
 #include "gstv4l2transform.h"
 
@@ -168,6 +170,10 @@ gst_v4l2_probe_and_register (GstPlugin * plugin)
     if (gst_v4l2_is_transform (sink_caps, src_caps))
       ret = gst_v4l2_transform_register (plugin, basename, it->device_path,
           sink_caps, src_caps);
+    else if (gst_v4l2_is_video_enc (sink_caps, src_caps))
+      ret = gst_v4l2_video_enc_register (plugin, basename, it->device_path,
+          sink_caps, src_caps);
+    /* else if ( ... etc. */
 
     gst_caps_unref (sink_caps);
     gst_caps_unref (src_caps);
